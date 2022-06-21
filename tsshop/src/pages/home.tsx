b7a961bc5556725ec9c2397df958518dfdeee5bb
mawavial <panzerkovic@gmail.com>
2022-06-21 11:35:29 -0300

    refact routes; Update index.html; Adds new styles; change footer and header into html tags;

--- tsshop/src/pages/home.tsx
+++ tsshop/src/pages/home.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useUser } from "../auth/tokenParser";
+import IProduct from "../interfaces/IProduct";
 
 
 const Home: React.FC = () => {
     const user = useUser();
-    console.log('%c%s', 'color: #e60000', user.isVerified);
-    //TODO - check if user has verified email if not error message
+    const [stock, setStock] = useState<IProduct[]>();
+
+    let fetchStock = async () => {
+        try {
+            let response = await fetch("http://localhost:5289/api/Stock");
+            response = await response.json();
+            setStock(response as unknown as IProduct[]);
+        } catch (error) { }
+    };
+
+    useEffect(() => {
+        fetchStock();
+    })
 
     return (<div>
         <h1>Home</h1>
+        <h2>Mais</h2>
+        <h2>Um</h2>
+        <h2>Mais</h2>
+        <h2>Dois</h2>
+        <h2>Mais</h2>
+        <h2>Tres</h2>
+        {stock.map((iten: any) => {
+            return <p>{iten}</p>
+        })}
+        <span>Outro</span>
+        <span>Outro</span>
+
         <div>
             {!user.isVerified ? <h2>You need to verify your email</h2> : <></>}
         </div>
